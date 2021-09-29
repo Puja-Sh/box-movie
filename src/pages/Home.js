90d9210852db49ecea1b9f00001347c232d25f15
puja <pujashm6@gmail.com>
2021-09-29 23:12:01 +0530

    implemented api; input search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>Home page</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const searchHandler = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  };
+
+  const onInputChangeHandler = e => {
+    setInput(e.target.value);
+  };
+
+  const onKeyDownHandler = e => {
+    if (e.keyCode === 13) {
+      searchHandler();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChangeHandler}
+        onKeyDown={onKeyDownHandler}
+        value={input}
+      />
+      <button type="button" onClick={searchHandler}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 }
 
 export default Home;
