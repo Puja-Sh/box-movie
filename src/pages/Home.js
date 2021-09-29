31962ed2d70bdbfc38d5e37f54279a065b1a3945
puja <pujashm6@gmail.com>
2021-09-29 23:51:57 +0530

    radio btn funtionality; actor api response

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ import { apiGet } from '../misc/config';
 function Home() {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [radioOptionSearch, setradioOptionSearch] = useState('shows');
+
+  const selectOption = radioOptionSearch === 'shows';
 
   const searchHandler = () => {
     apiGet(`/search/shows?q=${input}`).then(res => {
@@ -23,19 +26,21 @@ function Home() {
     }
   };
 
+  const onRadioChangeHandler = e => {
+    setradioOptionSearch(e.target.value);
+  };
+
   const renderResults = () => {
     if (results && results.length == 0) {
       return <div>No results, Sorry!</div>;
     }
 
     if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name} </div>
-          ))}
-        </div>
-      );
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name} </div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name} </div>
+          ));
     }
     return null;
   };
@@ -48,6 +53,30 @@ function Home() {
         onKeyDown={onKeyDownHandler}
         value={input}
       />
+
+      <div>
+        <label htmlFor="shows-search">
+          <input
+            type="radio"
+            id="shows-search"
+            value="shows"
+            checked={selectOption}
+            onChange={onRadioChangeHandler}
+          ></input>
+          Movies
+        </label>
+        <label htmlFor="actors-search">
+          <input
+            type="radio"
+            id="actors-search"
+            value="people"
+            checked={!selectOption}
+            onChange={onRadioChangeHandler}
+          ></input>
+          Actors
+        </label>
+      </div>
+
       <button type="button" onClick={searchHandler}>
         Search
       </button>
